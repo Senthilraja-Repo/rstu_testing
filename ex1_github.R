e039e6c656d27946f29418a99b00f8771d8c1555
Senthilraja-Repo <msenthil.mit@gmail.com>
2020-07-09 08:25:47 +0530

    Testing for new modification

--- ex1_github.R
+++ ex1_github.R
@@ -1,3 +1,4 @@
 print("This file is created in RStudio")
 print("and now it is stored in Github")
 Print("Additional information has added from first commit")
+Print("Updated new information on 09-07-2020")
